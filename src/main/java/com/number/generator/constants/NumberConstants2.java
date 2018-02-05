0f08eb7dc92295ffe3da0a39d821351fbc95f8ab
amarenderpaspanuri <30456199+amarenderpaspanuri@users.noreply.github.com>
2018-02-06 07:56:13 +1000

    Add files via upload

---
+++ src/main/java/com/number/generator/constants/NumberConstants2.java
@@ -0,0 +1,12 @@
+package com.number.generator.constants;
+
+public class NumberConstants2 {
+
+	public static final int TOTAL_NUMBERS = 45;
+	public static final int MAX_NUMBERS_PER_ROW = 6;
+	public static final int MAX_ROWS = 3;
+	
+	public static final boolean DUPLICATES_ALLOWED = false;
+	
+	
+}
