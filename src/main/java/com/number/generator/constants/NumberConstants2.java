a14db0e82496fac459adff930e4cf6fd01e4acfe
amarenderpaspanuri <30456199+amarenderpaspanuri@users.noreply.github.com>
2018-02-10 09:38:32 +1000

    Update NumberConstants2.java

--- src/main/java/com/number/generator/constants/NumberConstants2.java
+++ src/main/java/com/number/generator/constants/NumberConstants2.java
@@ -4,9 +4,9 @@ public class NumberConstants2 {
 
 	public static final int TOTAL_NUMBERS = 45;
 	public static final int MAX_NUMBERS_PER_ROW = 6;
-	public static final int MAX_ROWS = 3;
+	public static final int MAX_ROWS = 1;
 	
 	public static final boolean DUPLICATES_ALLOWED = false;
 	
-	
+	public static final int FINAL_ROW_COUNT = 45;
 }
