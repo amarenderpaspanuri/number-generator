3d1e9130c4c24fc66741ddd046bad7c07e30b9d1
amarenderpaspanuri <30456199+amarenderpaspanuri@users.noreply.github.com>
2018-02-12 07:53:42 +1000

    Update RowNumbers.java

--- src/main/java/com/number/generator/util/RowNumbers.java
+++ src/main/java/com/number/generator/util/RowNumbers.java
@@ -38,9 +38,10 @@ public final class RowNumbers {
 				}
 			}
 			if(extraColumnCount > 0) {
-				Random random = new Random();
+				//Random random = new Random();
 				count = rowCount * columnCount;
 				while (count < TOTAL_NUMBERS) {
+					Random random = new Random();
 					int[] row = numbers[random.nextInt(numbers.length)];
 					int index = random.nextInt(row.length - columnCount) + columnCount;
 					int number = row[index];
@@ -68,13 +69,15 @@ public final class RowNumbers {
 		int rowCount = 1;
 		while (rowCount <= MAX_ROWS) {
 			final List<Integer> row = new ArrayList<Integer>();
-			Random random = new Random();
+			//Random random = new Random();
 			int count = 1;
 			while (count <= MAX_NUMBERS_PER_ROW) {
+				Random random = new Random();
 				int rowIndex = random.nextInt(numbers.length);
 				int columnIndex = 0;
 				int number = numbers[rowIndex][columnIndex];
 				while (number == 0) {
+					random = new Random();
 					rowIndex = random.nextInt(numbers.length);
 					number = numbers[rowIndex][columnIndex];
 				}
@@ -95,14 +98,16 @@ public final class RowNumbers {
 		int rowCount = 1;
 		while (rowCount <= MAX_ROWS) {
 			final List<Integer> row = new ArrayList<Integer>();
-			Random random = new Random();
+			//Random random = new Random();
 			int count = 1;
 			while (count <= MAX_NUMBERS_PER_ROW) {
+				Random random = new Random();
 				int rowIndex = random.nextInt(numbers.length);
 				int columnIndex = random.nextInt(numbers[rowIndex].length);
 
 				int number = numbers[rowIndex][columnIndex];
 				while (number == 0) {
+					random = new Random();
 					columnIndex = random.nextInt(numbers[rowIndex].length);
 					number = numbers[rowIndex][columnIndex];
 				}
