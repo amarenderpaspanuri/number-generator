0d4ca826c9dd3e2be1ae72493557c14b58628e66
amarenderpaspanuri <30456199+amarenderpaspanuri@users.noreply.github.com>
2018-02-22 18:38:34 +1000

    Update NumberConstants2.java

--- src/main/java/com/number/generator/constants/NumberConstants2.java
+++ src/main/java/com/number/generator/constants/NumberConstants2.java
@@ -2,14 +2,14 @@ package com.number.generator.constants;
 
 public class NumberConstants2 {
 
-	public static final int MAX_NUMBERS = 45;
+	public static final int MAX_NUMBERS = 40;
 	public static final int MAX_NUMBERS_PER_ROW = 7;
 	
 	public static final int MAX_ROWS = 1;
 	
 	public static final boolean DUPLICATES_ALLOWED = false;
 	
-	public static final int ROWS_REQUIRED = 50;
+	public static final int ROWS_REQUIRED = 24;
 	
 	public static final int MAX_SUPPLEMTARY_NUMBER = 20;
 	
