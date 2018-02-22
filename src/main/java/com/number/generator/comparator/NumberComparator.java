6fb1396601b501208a0deac4df4dc7c22bbb2a40
amarenderpaspanuri <30456199+amarenderpaspanuri@users.noreply.github.com>
2018-02-22 18:45:13 +1000

    Add files via upload

---
+++ src/main/java/com/number/generator/comparator/NumberComparator.java
@@ -0,0 +1,15 @@
+package com.number.generator.comparator;
+
+import java.util.Comparator;
+
+public class NumberComparator implements Comparator<Integer> {
+
+	public int compare(Integer number1, Integer number2) {
+		if (number2 == number1)
+			return 0;
+		else if (number2 > number1)
+			return 1;
+		else
+			return -1;
+	}
+}
