47af4e70713668a8ff441d2b482a7fe7414d07d0
amarenderpaspanuri <30456199+amarenderpaspanuri@users.noreply.github.com>
2018-04-18 15:40:38 +1000

    Update NumberComparator.java

--- src/main/java/com/number/generator/comparator/NumberComparator.java
+++ src/main/java/com/number/generator/comparator/NumberComparator.java
@@ -7,7 +7,7 @@ public class NumberComparator implements Comparator<Integer> {
 	public int compare(Integer number1, Integer number2) {
 		if (number2 == number1)
 			return 0;
-		else if (number2 > number1)
+		else if (number1 > number2)
 			return 1;
 		else
 			return -1;
